b1d482464f52b099397eec5570a5da949b719899
John <john@datawave.ro>
2017-09-08 14:43:08 +0300

    Login / Logout functionality

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './rootReducer';
+import { userLoggedIn } from './actions/auth';
 
 const store = createStore(
     rootReducer,
     composeWithDevTools(applyMiddleware(thunk))
 )
+
+if (localStorage.bookwormJWT) {
+    const user = {token: localStorage.bookwormJWT};
+    store.dispatch(userLoggedIn(user));
+}
 
 ReactDOM.render(
     <BrowserRouter>
